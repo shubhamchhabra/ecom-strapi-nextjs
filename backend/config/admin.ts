f752385572d54a5f1badbc67c946bed07cc4e43b
Shubham Chhabra <shubhamchhabra222@gmail.com>
2024-10-13 14:32:55 +0530

    ttokens

--- backend/config/admin.ts
+++ backend/config/admin.ts
@@ -1,13 +1,13 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','NB9VAOnoIrJIPxw9n8uj2g=='),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT', '/TzAbage08AjCeWk52oBYA=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT','Nz8Sik9MrsylPVMh1nGJEQ=='),
     },
   },
   flags: {
