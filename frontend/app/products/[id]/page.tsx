7a2b395c246975e0db20605ef2f7c451dac5475f
Shubham Chhabra <shubhamchhabra222@gmail.com>
2024-10-13 15:08:49 +0530

    env

--- frontend/app/products/[id]/page.tsx
+++ frontend/app/products/[id]/page.tsx
@@ -12,14 +12,13 @@ export default function ProductDetailPage() {
   const router = useRouter();
   const params = useParams();
   const documentId = params?.id;
-  const baseUrl = "https://ecom-strapi-nextjs.onrender.com" || "http://localhost:1337";
   useEffect(() => {
     if (!documentId) return;
 
     async function fetchProduct() {
       setLoading(true);
       try {
-        const response = await fetch(`${baseUrl}/api/products/${documentId}?populate=*`);
+        const response = await fetch(`${process.env.API_URL}/api/products/${documentId}?populate=*`);
         if (!response.ok) throw new Error("Product not found");
         const res = await response.json();
         setProduct(res?.data);
@@ -40,7 +39,7 @@ export default function ProductDetailPage() {
     <main className="p-8">
       <div className="flex flex-col md:flex-row items-start gap-8">
         <Image
-          src={`${baseUrl}${product?.Image?.url}`}
+          src={`${process.env.API_URL}${product?.Image?.url}`}
           alt={product?.Name}
           className="w-full md:w-1/2 h-auto object-cover"
         />
