986d0ae2644391d50aee9f7ce10f2a91409cb5eb
Shubham Chhabra <shubhamchhabra222@gmail.com>
2024-10-15 23:47:46 +0530

    db

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,12 +1,12 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: env('DATABASE_CLIENT', 'sqlite'),
+    client: env('DATABASE_CLIENT', 'postgres'),
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'dpg-cs7b2t23esus73cj29g0-a'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
+      database: env('DATABASE_NAME', 'strapi_ecom_68qs'),
+      user: env('DATABASE_USERNAME', 'strapi_ecom_68qs_user'),
+      password: env('DATABASE_PASSWORD', 'WJplwZfQj8MoEsneR6oKOVX8Ygk3PG3q'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
